ba30583dc264040d4c7eaeac1e05a36e7194b7ec
Patrik Lundin <patrik.lundin@tink.com>
2023-05-24 18:20:11 +0200

    get components and post incident

---
+++ mail_forwarder/forwarder.py
@@ -0,0 +1,44 @@
+import json
+import requests
+import pprint
+
+getComponentsURL = "https://api.statuspage.io/v1/pages/n814cl79vp6c/components"
+headers = {"Authorization": "OAuth dc20ab8b-cb02-4fff-9374-e50635bb0e7c"}
+
+
+r = requests.get(url=getComponentsURL, headers=headers)
+data = r.json()
+
+print(r.json)
+
+components = {}
+for obj in data:
+    components[obj["name"]] = obj["id"]
+
+# print(components)
+
+
+# match name from email tokenizer
+
+
+# would also need to map service provider names in order to match for example BEC, Bankdata and SDC
+
+name = "Swedbank"
+
+componentId = components[name]
+
+postIncidentURL = "https://api.statuspage.io/v1/pages/n814cl79vp6c/incidents"
+headers = {"Authorization": "OAuth dc20ab8b-cb02-4fff-9374-e50635bb0e7c"}
+data = {
+    "incident": {
+        "name": "Bank Down",
+        "status": "investigating",
+        "body": "Everythiing is down",
+        "component_ids": [componentId],
+    }
+}
+
+
+r = requests.post(url=postIncidentURL, json=data, headers=headers)
+
+print(r.text)
